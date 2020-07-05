52a0de38868e8c9d221eec4fbec13d4f701518c4
nicolefernandes18 <37707051+nicolefernandes18@users.noreply.github.com>
2020-07-05 20:07:46 +0530

    index.html updated

--- tshirt/tshirt/settings.py
+++ tshirt/tshirt/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tshirtapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- tshirt/tshirtapp/views.py
+++ tshirt/tshirtapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('tshirtapp/index.html')
+    
     context = {
         
     }
-    return HttpResponse(template.render(context,request))
+    return render(request, 'tshirtapp/index.html', context)
+
