df66cc49ea182f60992ea372cc4f433fb8a55443
akhilshetty97 <47709446+akhilshetty97@users.noreply.github.com>
2020-07-12 16:19:48 +0530

    DBs setup

--- tshirt/tshirtapp/models.py
+++ tshirt/tshirtapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tshirt(models.Model):
+
+    FirstName = models.CharField(max_length=100)
+    LastName =  models.CharField(max_length=100)
+    Email =  models.CharField(max_length=100)
+    Password = models.CharField(max_length=100)
+    ConfirmPassword = models.CharField(max_length=100)
