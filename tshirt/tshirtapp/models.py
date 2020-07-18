cb2fec32ed914e7184a1bcd739d77730ed282280
akhilshetty97 <47709446+akhilshetty97@users.noreply.github.com>
2020-07-18 17:11:45 +0530

    signupnew
    signupnew

--- tshirt/tshirtapp/models.py
+++ tshirt/tshirtapp/models.py
@@ -1,16 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+
 
 # Create your models here.
-class Tshirt(AbstractBaseUser):
+
+class Tshirt(models.Model):
+
 
     FirstName = models.CharField(max_length=100)
     LastName =  models.CharField(max_length=100)
-    Email =  models.CharField(max_length=100, unique=True)
+    Username = models.CharField(max_length=100)
+    Email =  models.EmailField(max_length=100,unique=True)
     Password = models.CharField(max_length=100)
     ConfirmPassword = models.CharField(max_length=100)
 
-    REQUIRED_FIELDS = ['Password']
-    USERNAME_FIELD = 'Email'
+    
+
 
     
--- tshirt/tshirtapp/views.py
+++ tshirt/tshirtapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Tshirt
-from django.contrib.auth.models import auth
+from django.contrib.auth.models import User , auth
 
 # Create your views here.
 def index(request):
@@ -11,53 +11,58 @@ def index(request):
     return render(request, 'tshirtapp/index.html', context)
 
 def signup(request):
-
+    
     if request.method == 'POST':
         firstname = request.POST['firstname']
         lastname = request.POST['lastname']
+        username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
         cpassword = request.POST['cpassword']
 
-        tshirt_obj = Tshirt(FirstName = firstname, LastName=lastname, Email=email , Password=password , ConfirmPassword=cpassword )
-
+        
         if password==cpassword:
-            if Tshirt.objects.filter(Email=email).exists():
-                print("Email taken")
-
+            if User.objects.filter(username=username).exists():
+                print("Username taken")
+            elif User.objects.filter(email=email).exists():
+                print('Email taken')
             else:
-                tshirt_obj.save()
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
                 print('CREATED')
-            
-            
-
         else:
             print("Password not matching..")
 
         return redirect('index')
 
     else:
+    
         context1 = {
 
-        }
+        }   
         return render(request,'tshirtapp/signup.html',context1 )
 
 def login(request):
 
+    """
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
 
-        tshirt_obj = auth.authenticate(Email=email, Password=password)
+        #tshirt_obj = Tshirt( Email=email )
+
+        
+        #tshirt_obj = auth.authenticate(Email=email, Password=password)
 
         if tshirt_obj is not None:
             auth.login(request, tshirt)
             return redirect('index')
         else:
             print('Invalid credentials')
-            return redirect('login')
+            
+        return redirect('login')
     else:
-        context2 = {
-
-        }
-        return render(request,'tshirtapp/login.html',context2)
+    """
+    context2 = {
+    }
+    return render(request,'tshirtapp/login.html',context2)
