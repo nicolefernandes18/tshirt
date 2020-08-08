5fd173b65edeaa8687c0ad85117f1986a6987f37
akhilshetty97 <47709446+akhilshetty97@users.noreply.github.com>
2020-08-08 16:51:31 +0530

    men model

--- tshirt/tshirtapp/models.py
+++ tshirt/tshirtapp/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 # Create your models here.
 
-#class Men(models.Model):
+class Men(models.Model):
 
 
-    #Name = models.CharField(max_length=100)
-    #Brand =  models.CharField(max_length=100)
-    #Size = models.CharField(max_length=100)
-    #Price =  models.CharField(max_length=100)
-    #Image= models.CharField(max_length=100)
+    Name = models.CharField(max_length=100)
+    Brand =  models.CharField(max_length=100)
+    Size = models.CharField(max_length=100)
+    Price =  models.CharField(max_length=100)
+    Image= models.ImageField(upload_to='pics')
     
 
     
