38633e7dcdd4f524461983c9859d5bb3bf76e4a4
nicolefernandes18 <37707051+nicolefernandes18@users.noreply.github.com>
2020-08-08 17:27:52 +0530

    Women added

--- tshirt/tshirtapp/admin.py
+++ tshirt/tshirtapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Men
+from .models import Men, Women
 
 # Register your models here.
 
 admin.site.register(Men)
+admin.site.register(Women)
--- tshirt/tshirtapp/models.py
+++ tshirt/tshirtapp/models.py
@@ -12,7 +12,14 @@ class Men(models.Model):
     Price =  models.CharField(max_length=100)
     Image= models.ImageField(upload_to='pics')
     
+class Women(models.Model):
 
+
+    W_Name = models.CharField(max_length=100)
+    W_Brand =  models.CharField(max_length=100)
+    W_Size = models.CharField(max_length=100)
+    W_Price =  models.CharField(max_length=100)
+    W_Image= models.ImageField(upload_to='w_pics')
     
 
 
